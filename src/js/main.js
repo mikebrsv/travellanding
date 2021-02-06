4254c47fd478513ccb5400a2c72f0d5d9b737308
Mikhail Borisov <mikebrsv@gmail.com>
2021-02-07 00:26:14 +0300

    Refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import '../sass/style.sass';
 
 const menuToggle = document.querySelector('.toggle')
-const showcase = document.querySelector('.showcase')
+const content = document.querySelector('.content')
+const menu = document.querySelector('.menu')
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active')
-  showcase.classList.toggle('active')
+  content.classList.toggle('active')
+  menu.classList.toggle('active')
 })
