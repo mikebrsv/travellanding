52f48482b3f3f9fd5b7dacd431229edc525c1e5f
Mikhail Borisov <mikebrsv@gmail.com>
2021-02-07 03:26:49 +0300

    Menu mobile bug fixed, Menu position fixed

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,1 @@
-!function(){"use strict";const e=document.querySelector(".toggle"),t=document.querySelector(".content"),c=document.querySelector(".menu");e.addEventListener("click",(()=>{e.classList.toggle("active"),t.classList.toggle("active"),c.classList.toggle("active")}))}();
+!function(){"use strict";const e=document.querySelector(".toggle"),t=document.querySelector(".content-main"),c=document.querySelector(".menu");e.addEventListener("click",(()=>{e.classList.toggle("active"),t.classList.toggle("active"),c.classList.toggle("active")}))}();
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 import '../sass/style.sass';
 
 const menuToggle = document.querySelector('.toggle')
-const content = document.querySelector('.content')
+const contentMain = document.querySelector('.content-main')
 const menu = document.querySelector('.menu')
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active')
-  content.classList.toggle('active')
+  contentMain.classList.toggle('active')
   menu.classList.toggle('active')
 })
