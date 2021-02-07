de4b0c75a610cc253270f8422ee08ff2696ecdad
Mikhail Borisov <mikebrsv@gmail.com>
2021-02-07 05:05:10 +0300

    menuToggle change color on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,27 @@
-import '../sass/style.sass';
+import '../sass/style.sass'
 
 const menuToggle = document.querySelector('.toggle')
 const contentMain = document.querySelector('.content-main')
 const menu = document.querySelector('.menu')
+const showcase = document.querySelector('.showcase')
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active')
   contentMain.classList.toggle('active')
   menu.classList.toggle('active')
 })
+
+const isInViewport = function (elem) {
+  const bounding = elem.getBoundingClientRect()
+  return (
+    bounding.bottom <= 0
+  )
+}
+
+window.addEventListener('scroll', function (event) {
+  if (isInViewport(showcase)) {
+    menuToggle.classList.add('dark')
+  } else {
+    menuToggle.classList.remove('dark')
+  }
+}, false)
